c7caead24fbddc6b00f542129184d9ff0a355179
Tommy P. Keane <talk.to.tommypkeane@gmail.com>
2023-08-16 23:37:51 -0400

    [UPD] TofuFlaneur - New Items and Sprites

--- TofuFlaneur/_main/2DGame.gd
+++ TofuFlaneur/_main/2DGame.gd
@@ -13,7 +13,23 @@ func _ready():
 
 func start():
 	show()
+	
 	$Tofu.initialize($UserStartPosition.position)
+	
+	$ItemCashew.position = $UserStartPosition.position + Vector2(128 + 256, 32)
+	$ItemCashew.show()
+	$ItemCashew.apply_scale(Vector2(4, 4))
+	$ItemCashew.get_child(1).play()
+	
+	$ItemAcorn.position = $UserStartPosition.position + Vector2(128 + 512, 32)
+	$ItemAcorn.show()
+	$ItemAcorn.apply_scale(Vector2(4, 4))
+	$ItemAcorn.get_child(1).play()
+	
+	$ItemAlmond.position = $UserStartPosition.position + Vector2(128 + 768, 32)
+	$ItemAlmond.show()
+	$ItemAlmond.apply_scale(Vector2(4, 4))
+	$ItemAlmond.get_child(1).play()
 	return
 
 # Called every frame
